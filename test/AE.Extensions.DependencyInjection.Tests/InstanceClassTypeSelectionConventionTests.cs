6371d52986a841aa556c39fc54a68103da5a4902
Wojciech Gadziński <wojtekgadzinski@gmail.com>
2016-02-16 22:50:19 +0100

    Add tests for instanceClass conventions

---
+++ test/AE.Extensions.DependencyInjection.Tests/InstanceClassTypeSelectionConventionTests.cs
@@ -0,0 +1,69 @@
+﻿namespace AE.Extensions.DependencyInjection.Tests
+{
+    using System.Collections.Generic;
+
+    using Builder.Conventions;
+
+    using TestServices.Abstract;
+
+    using Xunit;
+
+    public class InstanceClassTypeSelectionConventionTests
+    {
+        [Fact]
+        public void Convention_select_any_default_class_type()
+        {
+            // Arrange
+            var type = typeof(object);
+            var convention = new InstanceClassTypeSelectionConvention();
+
+            // Act
+            var isSelected = convention.DoesSelect(type);
+
+            // Assert
+            Assert.True(isSelected);
+        }
+
+        [Fact]
+        public void InstanceClass_convention_doesnt_select_abstract_class()
+        {
+            // Arrange
+            var abstractType = typeof(AbstractClass);
+            var convention = new InstanceClassTypeSelectionConvention();
+
+            // Act
+            var isNotSelected = convention.DoesSelect(abstractType);
+
+            // Assert
+            Assert.False(isNotSelected);
+        }
+
+        [Fact]
+        public void InstanceClass_convention_doesnt_select_value_type()
+        {
+            // Arrange
+            var abstractType = typeof(int);
+            var convention = new InstanceClassTypeSelectionConvention();
+
+            // Act
+            var isNotSelected = convention.DoesSelect(abstractType);
+
+            // Assert
+            Assert.False(isNotSelected);
+        }
+
+        [Fact]
+        public void InstanceClass_convention_doesnt_select_generic_definition_type()
+        {
+            // Arrange
+            var abstractType = typeof(IEnumerable<>);
+            var convention = new InstanceClassTypeSelectionConvention();
+
+            // Act
+            var isNotSelected = convention.DoesSelect(abstractType);
+
+            // Assert
+            Assert.False(isNotSelected);
+        }
+    }
+}
---
+++ test/AE.Extensions.DependencyInjection.Tests/TestServices/Abstract/AbstractClass.cs
@@ -0,0 +1,6 @@
+﻿namespace AE.Extensions.DependencyInjection.Tests.TestServices.Abstract
+{
+    public abstract class AbstractClass
+    {
+    }
+}
